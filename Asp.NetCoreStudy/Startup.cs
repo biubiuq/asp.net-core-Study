da7765d6f8271e9a83398b024c6f1f332ff4624c
biubiuqq <495068093@qq.com>
2020-12-06 23:49:37 +0800

    1111

--- Asp.NetCoreStudy/Startup.cs
+++ Asp.NetCoreStudy/Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Asp.NetCoreStudy.db;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -10,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Asp.NetCoreStudy
 {
@@ -34,6 +37,25 @@ namespace Asp.NetCoreStudy
             services.AddControllers();
             var connection = Configuration.GetSection("MysqlConnection").Value;
             services.AddDbContext<mysqlContext>(options => options.UseMySql(connection));
+            services.AddAuthentication(config =>
+            {
+                //认证方案设置为Jwt
+                config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(config =>
+            {
+                config.RequireHttpsMetadata = false;
+                config.SaveToken = true;  //保存token
+                config.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = false,//不验证签发人
+                    ValidateAudience = false,  //不验证听众
+                    ValidateIssuerSigningKey = true, //验证签发者密钥
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("")) //签发者密钥
+                };
+            });
+            //将生成token的类注册为单例
+         //   services.AddSingleton<IJwtAuthenticationHandler>(new JwtAuthenticationHandler(tokenSecretKey));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
